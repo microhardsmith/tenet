c774fa2a788c59752fc5e46b79db3e7c2f8e5054
noway <710842092@qq.com>
2023-01-31 16:02:21 +0800

    Add c skeleton codes

---
+++ common/src/main/c/windows/lib.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "wepoll.h"
+
+#pragma comment (lib, "ws2_32.lib")
+
+int main() {
+    printf("hello");
+}
---
+++ common/src/main/java/cn/zorcc/common/NativeHelper.java
@@ -0,0 +1,13 @@
+package cn.zorcc.common;
+
+import java.lang.invoke.MethodHandle;
+
+/**
+ *  Helper class when need to reach C native methods
+ */
+public class NativeHelper {
+    public static MethodHandle getNativeMethod() {
+        // TODO
+        return null;
+    }
+}
